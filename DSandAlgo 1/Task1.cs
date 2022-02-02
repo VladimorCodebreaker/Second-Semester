29059c7fff337f65ae4b99052c6c29af1700e3c7
Vladimir Schmadlak <vschmadlak@googlemail.com>
2022-02-02 19:15:18 +0200

    Hi, wassup

---
+++ DSandAlgo 1/Task1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSandAlgo_1
+{
+    class Task1
+    {
+        public static void Method()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
