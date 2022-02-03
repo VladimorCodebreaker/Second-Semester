f1735cf39b3eb0252adf119367e9e74a8bcfb1fc
Vladimir Schmadlak <vschmadlak@googlemail.com>
2022-02-03 13:07:42 +0200

    DS and Algo Homework Ex.2, Ex.3 Lecture 1

---
+++ Second-Semester/DSandAlgo 1/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSandAlgo_1
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int StudentNumber { get; set; }
+    }
+}
