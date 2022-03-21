71c5679d847296e514f52841d5e70cd051df5bb4
Vladimir Schmadlak <91912841+VladimorCodebreaker@users.noreply.github.com>
2022-03-21 22:16:50 +0200

    Homew 1-5 DS and Algo

---
+++ DSandAlgo 7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSandAlgo_7.Lecture;
+using DSandAlgo_7.Homework;
+
+namespace DSandAlgo_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class1.Method();
+
+            //Task_1.SortedArray();
+
+            //Class1.Input();
+
+            //Task_2.Input();
+
+            //Task_3.Test();
+
+            //Task_4 task4 = new Task_4();
+            //task4.Test();
+
+            Task_5.Run();
+        }
+    }
+}
