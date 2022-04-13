eb77a3b51bd81f47db33a83e654d3c475b6797e5
Vladimir Schmadlak <91912841+VladimorCodebreaker@users.noreply.github.com>
2022-04-13 10:04:23 +0300

    CSV, JSON, XML

---
+++ Programming_7/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Programming_7
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
+
